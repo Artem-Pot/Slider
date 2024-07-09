d10de0e8932f0c94175781391942a4f2583ba7b7
Artem <blbo@yandex.ru>
2024-07-09 14:43:13 +0400

    0.0.1
    Начало

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -4,28 +4,31 @@ const stub = document.querySelectorAll('.stub'); // слайдеры
 const nextBotton = document.querySelector('.next'); //кнопка следующий
 const backBotton = document.querySelector('.back'); //кнопка предыдущий
 
-let numberSlider = 0;
 
-// function test() {
-//     for (let i = 0; i < slider.children.length; i++) {
-//         if(slider.children[i].classList.contains('stub_activ')) {
-//         slider.children[i].classList.remove('stub_activ');
-//         slider.children[i + 1].classList.add('stub_activ2');
-//         }        
-//     }
-// }
+let numberSlider = 0; //счетчик текущего слайдера
 
 nextBotton.addEventListener('click', () => {
-    slider.children[numberSlider].classList.remove('stub_activ');
-    slider.children[numberSlider + 1].classList.add('stub_activ');
-    numberSlider += 1;
-    console.log(numberSlider + 1);
+    if (numberSlider + 1 < slider.children.length){
+        slider.children[numberSlider].classList.remove('stub_activ');
+        slider.children[numberSlider + 1].classList.add('stub_activ');
+        numberSlider += 1;
+    }
+    else if(slider.children[numberSlider + 1] === undefined) {
+        slider.children[numberSlider].classList.remove('stub_activ');
+        slider.children[0].classList.add('stub_activ');
+        numberSlider = 0;
+    }
 });
 
 backBotton.addEventListener('click', () => {
-    slider.children[numberSlider].classList.remove('stub_activ');
-    slider.children[numberSlider - 1].classList.add('stub_activ');
-    numberSlider -= 1;
-    console.log(numberSlider + 1);    
+    // if (numberSlider + 1 <= slider.children.length){
+    //     slider.children[numberSlider].classList.remove('stub_activ');
+    //     slider.children[numberSlider - 1].classList.add('stub_activ');
+    //     numberSlider -= 1;
+    // }
+    // else if(slider.children[numberSlider - 1] === undefined) {
+    //     slider.children[numberSlider].classList.remove('stub_activ');
+    //     slider.children[slider.children.length].classList.add('stub_activ');
+    //     numberSlider = 0;
+    // }
 });
-
