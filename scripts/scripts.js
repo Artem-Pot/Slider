45d6a0e4a345ef7f4bbd2788646dec996ea93c20
Artem <blbo@yandex.ru>
2024-07-10 13:19:49 +0400

    0.0.6
    Анимация навигации слайдеров. + исправлен баг с кнопкой предыдущий слайдер.

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,8 +1,14 @@
 const slider = document.querySelector('.slider__box'); //список слайдеров
-const stub = document.querySelectorAll('.stub'); // слайдеры
+const sliderBox = document.querySelector('.slider__box'); //все слайдеры
+const stub = document.querySelectorAll('.stub'); // каждый элемент слайдера
 
 const nextButton = document.querySelector('.next'); //кнопка следующий
 const backButton = document.querySelector('.back'); //кнопка предыдущий
+
+
+const sliderNavigation = document.querySelector('.slider__navigation');
+
+
 
 //счетчик текущего слайдера
 let numberSlider = Number(0); 
@@ -32,16 +38,19 @@ function startSlider() {
         settings.autoSlider.slider = true;
         setInterval(nextSlider, settings.autoSlider.time);
     }
+
+    showNavigation();
 }
 
 //кнопка следующий слайдер
 nextButton.addEventListener('click', () => {
-    nextSlider();
+    nextSlider();    
 });
 
 //кнопка предыдущий слайдер
 backButton.addEventListener('click', () => {
     backSlider();
+    
 });
 
 //функция следующего слайда
@@ -49,11 +58,15 @@ function nextSlider() {
     if (numberSlider + 1 < slider.children.length){
         slider.children[numberSlider].classList.remove('stub_activ');
         slider.children[numberSlider + 1].classList.add('stub_activ');
+        sliderNavigation.children[numberSlider].classList.remove('slider__list_activ');
+        sliderNavigation.children[numberSlider + 1].classList.add('slider__list_activ');
         numberSlider += 1;
     }
     else if(slider.children[numberSlider + 1] === undefined) {
         slider.children[numberSlider].classList.remove('stub_activ');
         slider.children[0].classList.add('stub_activ');
+        sliderNavigation.children[numberSlider].classList.remove('slider__list_activ');
+        sliderNavigation.children[0].classList.add('slider__list_activ');
         numberSlider = 0;
     }
 }
@@ -63,14 +76,28 @@ function backSlider() {
     if (numberSlider > 0){
         slider.children[numberSlider].classList.remove('stub_activ');
         slider.children[numberSlider - 1].classList.add('stub_activ');
+        sliderNavigation.children[numberSlider].classList.remove('slider__list_activ');
+        sliderNavigation.children[numberSlider - 1].classList.add('slider__list_activ');
         numberSlider -= 1;
     }
     else if(slider.children[numberSlider - 1] === undefined) {
         numberSlider = slider.children.length - 1;
+        slider.children[0].classList.remove('stub_activ');
         slider.children[numberSlider].classList.add('stub_activ');
+        sliderNavigation.children[numberSlider - 1].classList.remove('slider__list_activ');
+        sliderNavigation.children[numberSlider].classList.add('slider__list_activ');
     }
+}
+
+// функция показа количества слайдов в виде навигации внизу 
+function showNavigation() {
+    for (let i = 0; i < sliderBox.children.length; i++) {
+        let item  = document.createElement('span');
+        item.classList.add('slider__list');
+        sliderNavigation.insertAdjacentElement("afterbegin", item );
+    }
+    // sliderNavigation.children[0].classList.remove('slider__list');
 }
 
 //запуск и проверка всех настроек слайдера
 startSlider();
-
