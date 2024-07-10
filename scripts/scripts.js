f6bde8e265ba1f9f7c943072833cf42ed5c0b4fb
Artem <blbo@yandex.ru>
2024-07-10 08:46:10 +0400

    0.0.4
    Добавлена функция автослайдера

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,13 +1,48 @@
 const slider = document.querySelector('.slider__box'); //список слайдеров
 const stub = document.querySelectorAll('.stub'); // слайдеры
 
-const nextBotton = document.querySelector('.next'); //кнопка следующий
-const backBotton = document.querySelector('.back'); //кнопка предыдущий
+const nextButton = document.querySelector('.next'); //кнопка следующий
+const backButton = document.querySelector('.back'); //кнопка предыдущий
 
 
-let numberSlider = Number(0); //счетчик текущего слайдера
 
-nextBotton.addEventListener('click', () => {
+
+//настройки
+const settings = {
+    slider : false, //вкл/вык автоматический слайдер
+    time: 3000, //количество секунд для запуска автоматического слайдера
+    
+};
+
+//счетчик текущего слайдера
+let numberSlider = Number(0); 
+
+//запуск и проверка всех настроек слайдера
+startSlider();
+
+//функция проверки и автоматического запуска слайдера через заданный промежуток времени
+function startSlider() {
+    //проверка и запуск автоматического слайдреа
+    if (settings.slider){
+        setInterval(nextSlider, settings.time);
+    }
+    else if (settings.slider === false) {
+        return;
+    }
+}
+
+//кнопка следующий слайдер
+nextButton.addEventListener('click', () => {
+    nextSlider();
+});
+
+//кнопка предыдущий слайдер
+backButton.addEventListener('click', () => {
+    backSlider();
+});
+
+//функция следующего слайда
+function nextSlider() {
     if (numberSlider + 1 < slider.children.length){
         slider.children[numberSlider].classList.remove('stub_activ');
         slider.children[numberSlider + 1].classList.add('stub_activ');
@@ -18,9 +53,10 @@ nextBotton.addEventListener('click', () => {
         slider.children[0].classList.add('stub_activ');
         numberSlider = 0;
     }
-});
+}
 
-backBotton.addEventListener('click', () => {
+//функция предыдущего слайда
+function backSlider() {
     if (numberSlider > 0){
         slider.children[numberSlider].classList.remove('stub_activ');
         slider.children[numberSlider - 1].classList.add('stub_activ');
@@ -30,7 +66,8 @@ backBotton.addEventListener('click', () => {
         numberSlider = slider.children.length - 1;
         slider.children[numberSlider].classList.add('stub_activ');
     }
-});
+}
+
 
 
 
