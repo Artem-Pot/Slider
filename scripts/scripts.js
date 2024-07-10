f54cff7152122f812ab1c66308f67c72b4b2f06f
Artem <blbo@yandex.ru>
2024-07-10 14:31:24 +0400

    0.0.7
    Дополнительные проверки и исправления ошибок конфликтов

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -5,10 +5,7 @@ const stub = document.querySelectorAll('.stub'); // каждый элемент 
 const nextButton = document.querySelector('.next'); //кнопка следующий
 const backButton = document.querySelector('.back'); //кнопка предыдущий
 
-
 const sliderNavigation = document.querySelector('.slider__navigation');
-
-
 
 //счетчик текущего слайдера
 let numberSlider = Number(0); 
@@ -17,29 +14,36 @@ let numberSlider = Number(0);
 let settings = {
     //автослайдер
     autoSlider : { 
-        slider : false, //вкл/вык автоматический слайдер
+        slider : 'off', //вкл/вык автоматический слайдер
         time: 1000, //количество секунд для запуска автоматической смены слайда
     },
     //стрелки next, back
     arrow : {
-        hiddenArrow : false, //скрыть кнопки следующий/предыдущий слайд. Запустится автослайдер.
+        arrow : 'on', //скрыть/показать кнопки следующий/предыдущий слайд. Если выключен то запустится автослайдер.
+    },
+    //нижняя навигация
+    navigation : {
+        navigation : 'off', //скрыть/показать навигацию
     },
 };
 
 //функция проверки и автоматического запуска слайдера через заданный промежуток времени
 function startSlider() {
     //проверка и запуск автоматического слайдера
-    if (settings.autoSlider.slider){
+    if (settings.autoSlider.slider === 'on'){
         setInterval(nextSlider, settings.autoSlider.time);
     }
-    if (settings.arrow.hiddenArrow === true) {
+    //проверка и запуск наличия кнопок вперед/назад
+    if (settings.arrow.arrow === 'off') {
         nextButton.style.display = 'none';
         backButton.style.display = 'none';
         settings.autoSlider.slider = true;
         setInterval(nextSlider, settings.autoSlider.time);
     }
-
-    showNavigation();
+    //проверка и запуск наличия нижней навигации
+    if (settings.navigation.navigation === 'on') {
+        showNavigation();
+    }
 }
 
 //кнопка следующий слайдер
@@ -58,15 +62,21 @@ function nextSlider() {
     if (numberSlider + 1 < slider.children.length){
         slider.children[numberSlider].classList.remove('stub_activ');
         slider.children[numberSlider + 1].classList.add('stub_activ');
-        sliderNavigation.children[numberSlider].classList.remove('slider__list_activ');
-        sliderNavigation.children[numberSlider + 1].classList.add('slider__list_activ');
+        //проверка если в настройках включена навигация
+        if (settings.navigation.navigation === 'on') {
+            sliderNavigation.children[numberSlider].classList.remove('slider__list_activ');
+            sliderNavigation.children[numberSlider + 1].classList.add('slider__list_activ');
+        }
         numberSlider += 1;
     }
     else if(slider.children[numberSlider + 1] === undefined) {
         slider.children[numberSlider].classList.remove('stub_activ');
         slider.children[0].classList.add('stub_activ');
-        sliderNavigation.children[numberSlider].classList.remove('slider__list_activ');
-        sliderNavigation.children[0].classList.add('slider__list_activ');
+        //проверка если в настройках включена навигация
+        if (settings.navigation.navigation === 'on') {
+            sliderNavigation.children[numberSlider].classList.remove('slider__list_activ');
+            sliderNavigation.children[0].classList.add('slider__list_activ');
+        }
         numberSlider = 0;
     }
 }
@@ -76,27 +86,35 @@ function backSlider() {
     if (numberSlider > 0){
         slider.children[numberSlider].classList.remove('stub_activ');
         slider.children[numberSlider - 1].classList.add('stub_activ');
-        sliderNavigation.children[numberSlider].classList.remove('slider__list_activ');
-        sliderNavigation.children[numberSlider - 1].classList.add('slider__list_activ');
+        //проверка если в настройках включена навигация
+        if (settings.navigation.navigation === 'on') {
+            sliderNavigation.children[numberSlider].classList.remove('slider__list_activ');
+            sliderNavigation.children[numberSlider - 1].classList.add('slider__list_activ');
+        }
+
         numberSlider -= 1;
     }
     else if(slider.children[numberSlider - 1] === undefined) {
         numberSlider = slider.children.length - 1;
         slider.children[0].classList.remove('stub_activ');
         slider.children[numberSlider].classList.add('stub_activ');
-        sliderNavigation.children[numberSlider - 1].classList.remove('slider__list_activ');
-        sliderNavigation.children[numberSlider].classList.add('slider__list_activ');
+        //проверка если в настройках включена навигация
+        if (settings.navigation.navigation === 'on') {
+            sliderNavigation.children[numberSlider - 1].classList.remove('slider__list_activ');
+            sliderNavigation.children[numberSlider].classList.add('slider__list_activ');
+        }
     }
 }
 
 // функция показа количества слайдов в виде навигации внизу 
 function showNavigation() {
+
     for (let i = 0; i < sliderBox.children.length; i++) {
         let item  = document.createElement('span');
         item.classList.add('slider__list');
         sliderNavigation.insertAdjacentElement("afterbegin", item );
     }
-    // sliderNavigation.children[0].classList.remove('slider__list');
+    sliderNavigation.children[numberSlider].classList.add('slider__list_activ');
 }
 
 //запуск и проверка всех настроек слайдера
